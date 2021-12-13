6a1461f2160fef93a1248f4cd59d71196610fdc4
Alper Reha Yazgan <alperreha@gmail.com>
2021-12-13 19:53:17 +0300

    initial commit

---
+++ meterology.py
@@ -0,0 +1,12 @@
+"""
+    YTU EMBEDDED SYSTEMS - METEROLOGY
+    RASPBERRY PI METEROLOGY APP
+"""
+
+# Definitions of sensor types and their corresponding GPIO pins
+
+# Register definitions that are used in the sensor data
+
+# Register listener function for the sensor data
+
+# Read All sensor data then send to server
