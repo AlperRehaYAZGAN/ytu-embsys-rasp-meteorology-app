9b5691b9c071cb7e6c19f38d93dba2d294490acf
Alper Reha Yazgan <alperreha@gmail.com>
2021-12-14 12:57:03 +0300

    dht 11 added

--- meterology.py
+++ meterology.py
@@ -5,6 +5,10 @@
 import RPi.GPIO as GPIO
 import time
 
+# DHT11 dependency
+import Adafruit_DHT
+sensor_DHT11 = Adafruit_DHT.DHT11
+
 # Definitions of sensor types and their corresponding GPIO pins
 dht11_pin = 4
 water_sensor_pin = 17
@@ -32,9 +36,16 @@ def setup():
 
 # Register listener function for the sensor data
 def read_dht11_data():
+    # Guideline : 
+    # git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+    # 1 - sudo apt-get install git-core
+    # 2 - sudo apt-get update
+    # 3 - sudo apt-get install build-essential python-dev
+    # 4 - sudo python setup.py install
+
     # Read the data from the sensor
-    dht11_data = GPIO.input(dht11_pin)
-    return dht11_data
+    humidity, temperature = Adafruit_DHT.read_retry(sensor_DHT11, dht11_pin)
+    return (humidity, temperature)
     pass
 
 def read_water_sensor_data():
@@ -61,14 +72,14 @@ def read_bmp180_data():
 def loop():
     while True:
         # Read the sensor data
-        dht11_data = read_dht11_data()
+        dht11_humidity, dht11_temp = read_dht11_data()
         water_sensor_data = read_water_sensor_data()
         mq5_data = read_mq5_data()
         ldr_data = read_ldr_data()
         bmp180_data = read_bmp180_data()
 
         # Print the sensor data
-        print("DHT11: " + str(dht11_data))
+        print("DHT11 Temperature = {0:0.1f} C Humidity = {1:0.1f} %".format(dht11_temp, dht11_humidity))
         print("Water Sensor: " + str(water_sensor_data))
         print("MQ5: " + str(mq5_data))
         print("LDR: " + str(ldr_data))
